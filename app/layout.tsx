99c7241a610d5e32ab105dd3e070a997b54103f2
clothingprojectnny-lab <clothingprojectnny@gmail.com>
2026-04-22 16:51:49 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   description: '',
   robots: { index: false, follow: false },
 }
-
+ 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
